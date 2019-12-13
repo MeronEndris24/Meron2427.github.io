158773073fe77363b546bfa65aed84582080efad
Meron2427 <33803258+Meron2427@users.noreply.github.com>
2019-12-12 21:48:15 -0600

    hm

---
+++ ModuleClosure/banking.js
@@ -0,0 +1,38 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+"use strict";
+let accountInfoList = [];
+
+let banking = (function() {
+    function Account(accountName, deposit) {
+        this.accountName = accountName;
+        this.deposit = deposit;
+
+        this.toStringx = function() {
+            return "Account name:  " + this.accountName + "  Balance:  " + this.deposit;
+        };
+    }
+
+    function updateTextArea() {
+        let textArea = document.getElementById('textArea');
+        textArea.value = "";
+        for(let i in accountInfoList) {
+            textArea.value += accountInfoList[i].toStringx() + "\n";
+        }
+    }
+
+    return {
+        createAccount: function() {
+            let accountName = document.getElementById("accountName").value;
+            let deposit = document.getElementById("deposit").value;
+            accountInfoList.push(
+                new Account(accountName, deposit)
+            );
+            updateTextArea();
+        }
+    };
+})();
